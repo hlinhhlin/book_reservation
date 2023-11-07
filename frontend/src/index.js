abd45c6ab9884192462217e3f6a30f945c15c2c2
hlinhhlin <120149572+hlinhhlin@users.noreply.github.com>
2023-11-07 15:06:29 +0700

    setup is done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'; // Import the App component
 import Home from './component/Home';
-import Second from './component/second';
 
-ReactDOM.render(<Second />, document.getElementById('root')); // Assuming you have a div with the id 'root' in your HTML
+ReactDOM.render(<Home />, document.getElementById('root')); // Assuming you have a div with the id 'root' in your HTML
