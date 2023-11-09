f5bcbb263e11843f0f5550521b51b963c07977b2
perthpperth <6422781524@g.siit.tu.ac.th>
2023-11-09 20:21:00 +0700

    Search

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -30,7 +30,31 @@ function HomePage() {
             <img src="bookcover3.jpg" alt="Book 3"/>
             <p className="genre">Romance</p>
         </div>
-    </div>
+        <div className="book-item">
+            <img src="bookcover4.jpg" alt="Book 4"/>
+            <p className="genre">Fantasy</p>
+        </div>
+        <div className="book-item">
+            <img src="bookcover5.jpg" alt="Book 5"/>
+            <p className="genre">Thriller</p>
+        </div>
+        <div className="book-item">
+            <img src="bookcover6.jpg" alt="Book 6"/>
+            <p className="genre">Horror</p>
+        </div>
+        <div className="book-item">
+            <img src="bookcover7.jpg" alt="Book 7"/>
+            <p className="genre">History</p>
+        </div>
+        <div className="book-item">
+            <img src="bookcover8.jpg" alt="Book 8"/>
+            <p className="genre">Biography</p>
+        </div>
+        <div className="book-item">
+            <img src="bookcover9.jpg" alt="Book 9"/>
+            <p className="genre">Kids</p>
+        </div>
+    </div>   
   );
 }
 
