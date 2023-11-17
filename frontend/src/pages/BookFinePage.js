25bd7084db7a9cdacdff056532bb2d6e1758ae2e
hlinhhlin <120149572+hlinhhlin@users.noreply.github.com>
2023-11-17 21:13:53 +0700

    BookFine, Transaction

--- frontend/src/pages/BookFinePage.js
+++ frontend/src/pages/BookFinePage.js
@@ -1,57 +1,91 @@
 import React, { useState, useEffect } from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useUser } from "../UserContext";
 import { FormatDate } from "../Config";
 
 const BookFinePage = () => {
-    const [bookData, setBookData] = useState([]);
-    const { user } = useUser();
+  const [bookData, setBookData] = useState([]);
+  const { user } = useUser();
 
-    const arrayBufferToBase64 = (buffer) => {
-        let binary = "";
-        const bytes = new Uint8Array(buffer);
-        for (let i = 0; i < bytes.byteLength; i++) {
-          binary += String.fromCharCode(bytes[i]);
-        }
-        return btoa(binary);
-      };
+  const arrayBufferToBase64 = (buffer) => {
+    let binary = "";
+    const bytes = new Uint8Array(buffer);
+    for (let i = 0; i < bytes.byteLength; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return btoa(binary);
+  };
 
-    useEffect(() => {
-        // Fetch transactions when the component mounts
-        fetch(`http://localhost:5050/user/book/fine/${user.id}`)
-          .then((response) => response.json())
-          .then((data) => {
-            setBookData(data.books);
-          })
-          .catch((error) => {
-            console.log("Error fetching transactions:", error);
-          });
-      }, []);
+  useEffect(() => {
+    // Fetch transactions when the component mounts
+    fetch(`http://localhost:5050/user/book/fine/${user.id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setBookData(data.books);
+      })
+      .catch((error) => {
+        console.log("Error fetching transactions:", error);
+      });
+  }, []);
 
-    return (
-        <Box>
-  {bookData &&
-    bookData.map(({ Title, BookImage, PenName, ReturnDate, Status, Amount }, index) => (
-      <div className="img-left-format" style={{ display: "flex" }}>
-        <img src={`data:image/jpeg;base64,${arrayBufferToBase64(BookImage.data)}`} alt="Book 2" />
-        <div className="text-format" style={{ marginLeft: "20px" }}>
-          <h4>Title: {Title}</h4>
-          <p>Author: {PenName}</p>
-          <div style={{ display: "flex" }}>
-            <div className="date-container">
-              <p>Return Date:</p>
-              <p>{FormatDate(ReturnDate)}</p>
-            </div>
-          </div>
+  return (
+    <Box>
+      {bookData.length === 0 ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "90vh",
+            color: "#757575"
+          }}
+        >
+          <Typography variant="h3">No Data</Typography>
         </div>
-        <h5 style={{ color: "red", marginTop: "200px", marginLeft: "600px" }}>{Amount} BAHT</h5>
-        {index < bookData.length - 1 && <hr />}
-        {/* Add horizontal line if it's not the last item */}
-      </div>
-    ))}
-</Box>
-    );
-
+      ) : (
+        bookData.map(
+          (
+            { Title, BookImage, PenName, ReturnDate, Status, Amount },
+            index
+          ) => (
+            <div
+              className="img-left-format"
+              style={{ display: "flex" }}
+              key={index}
+            >
+              <img
+                src={`data:image/jpeg;base64,${arrayBufferToBase64(
+                  BookImage.data
+                )}`}
+                alt="Book 2"
+              />
+              <div className="text-format" style={{ marginLeft: "20px" }}>
+                <h4>Title: {Title}</h4>
+                <p>Author: {PenName}</p>
+                <div style={{ display: "flex" }}>
+                  <div className="date-container">
+                    <p>Return Date:</p>
+                    <p>{FormatDate(ReturnDate)}</p>
+                  </div>
+                </div>
+              </div>
+              <h5
+                style={{
+                  color: "red",
+                  marginTop: "200px",
+                  marginLeft: "600px",
+                }}
+              >
+                {Amount} BAHT
+              </h5>
+              {index < bookData.length - 1 && <hr />}
+              {/* Add horizontal line if it's not the last item */}
+            </div>
+          )
+        )
+      )}
+    </Box>
+  );
 };
 
 export default BookFinePage;
--- frontend/src/pages/TransactionHistoryPage.js
+++ frontend/src/pages/TransactionHistoryPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useUser } from "../UserContext";
 import { FormatDate, capitalizeFirstLetter } from "../Config";
 
@@ -20,42 +20,65 @@ const TransactionHistoryPage = () => {
   }, []);
 
   return (
-<Box>
-  {transaction &&
-    transaction
-      .sort((a, b) => new Date(b.TransactionDate) - new Date(a.TransactionDate))
-      .map(({ TransactionDate, Status, Type, Amount }, index) => (
-        <div key={TransactionDate}>
-          <div style={{ display: "flex" }}>
-            <div
-              style={{
-                marginLeft: "50px",
-                marginTop: "50px",
-                lineHeight: "1.5",
-              }}
-            >
-              <p>{FormatDate(TransactionDate)}</p>
-              <p style={{ color: "#444444" }}>Type: {capitalizeFirstLetter(Type)}</p>
-              <p style={{ color: "#444444" }}>Status: {capitalizeFirstLetter(Status)}</p>
-            </div>
-            <div>
-              <h5
-                style={{
-                  color: Type === "fine" ? "red" : Type === "top-up" ? "green" : "black",
-                  marginTop: "100px",
-                  marginLeft: "1100px",
-                }}
-              >
-                {Amount} BAHT
-              </h5>
-            </div>
-          </div>
-          {index < transaction.length - 1 && <hr />} {/* Add horizontal line if it's not the last transaction */}
+    <Box>
+      {transaction.length === 0 ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "90vh",
+            color: "#757575",
+          }}
+        >
+          <Typography variant="h3">No Data</Typography>
         </div>
-      ))}
-</Box>
-
-  
+      ) : (
+        transaction
+          .sort(
+            (a, b) => new Date(b.TransactionDate) - new Date(a.TransactionDate)
+          )
+          .map(({ TransactionDate, Status, Type, Amount }, index) => (
+            <div key={TransactionDate}>
+              <div style={{ display: "flex" }}>
+                <div
+                  style={{
+                    marginLeft: "50px",
+                    marginTop: "50px",
+                    lineHeight: "1.5",
+                  }}
+                >
+                  <p>{FormatDate(TransactionDate)}</p>
+                  <p style={{ color: "#444444" }}>
+                    Type: {capitalizeFirstLetter(Type)}
+                  </p>
+                  <p style={{ color: "#444444" }}>
+                    Status: {capitalizeFirstLetter(Status)}
+                  </p>
+                </div>
+                <div>
+                  <h5
+                    style={{
+                      color:
+                        Type === "fine"
+                          ? "red"
+                          : Type === "top-up"
+                          ? "green"
+                          : "black",
+                      marginTop: "100px",
+                      marginLeft: "1100px",
+                    }}
+                  >
+                    {Amount} BAHT
+                  </h5>
+                </div>
+              </div>
+              {index < transaction.length - 1 && <hr />}{" "}
+              {/* Add horizontal line if it's not the last transaction */}
+            </div>
+          ))
+      )}
+    </Box>
   );
 };
 
