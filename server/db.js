886d03456ea18bcb522efc8c6ebe9762eb28b6ef
hlinhhlin <120149572+hlinhhlin@users.noreply.github.com>
2023-11-19 00:27:56 +0700

    improve database and api

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'root',
-  database: 'book-reservation',
+  database: 'book-reservation-new',
 });
 
 // Connect to the database;
